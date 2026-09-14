19ca1b4fa0712a03f9675c7b2a37d41e0d93383d
kastalco <kastalcowork@gmail.com>
2026-09-14 09:25:07 -0500

    Linktree kastalco info

--- src/index.js
+++ src/index.js
@@ -1,38 +1,121 @@
 const $links = document.getElementById('links');
-const name = document.querySelector('h1');
-
+const $name = document.querySelector('h1');
+const $userName = document.querySelector('h2');
+const $avatar = document.getElementById('avatar');
+const $description = document.getElementById('description');
+const setSocialMediaContainer = document.getElementById('social-media-container');
 
 const data = {
   name: "Kamilo Alomías",
   nickname: "kastalco",
-  description: "...",
-  avatar: "...",
+  description: "Senior Product Designer with +7 years of experience and Design Engineer with knowledge in HTML, CSS and JavaScript",
+  avatar: "https://media.licdn.com/dms/image/v2/D4E03AQGrNkOqyrIshw/profile-displayphoto-shrink_800_800/B4EZWH9ggQHMAc-/0/1741742803420?e=1790812800&v=beta&t=z2S6iYwkqOllWx-vPKIB232uABygJT_FIXEl6mj2CKk",
   social: [
     {
       name: "linkedId",
       url: "http://linkedin.com/in/kastalco",
       username: "kastalco",
+      icon: `<svg class="w-6 h-6 cursor-pointer text-gray-400 hover:text-blue-500 fill-current"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path
+                                        d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z">
+                                    </path>
+            </svg>`
     },
     {
       name: "github",
       url: "https://github.com/Kastalco97",
       username: "Kastalco97",
+      icon: `<svg class="w-6 h-6 cursor-pointer text-gray-400 hover:text-[#6e5494] fill-current"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path
+                                        d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            </svg>`
+    },
+    {
+      name: "platzi",
+      url: "https://platzi.com/p/kastalco/",
+      username: "@kastalco",
+      icon:`<svg class="w-6 h-6 cursor-pointer text-gray-400 hover:text-[#98CA3F] fill-current"
+                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M10.6392 1.127L2.486 9.282a3.842 3.842 0 000 5.4361l8.1552 8.1552a3.842 3.842 0 005.4361 0l2.719-2.718-2.719-2.7181-2.718 2.718-8.1562-8.1551 8.1552-8.1552 5.437 5.4371-5.437 5.4361 2.718 2.719 5.4371-5.437a3.842 3.842 0 000-5.4372l-5.448-5.436a3.828 3.828 0 00-5.4252 0z" />
+            </svg>`
     },
   ],
-  links: [
-    {
-      name: "blog",
-      url: "https://example.com/",
-      color: "red",
-      emoji: "📖",
+  links: [{
+      name: "ECMAScript",
+      url: "https://platzi.com/p/kastalco/curso/3504-course/diploma/detalle/",
+      color: "yellow",
+      emoji: "https://static.platzi.com/media/achievements/badge-escmascript-historia-92707485-a3f2-4946-91f0-fb0291f5887d.png",
+    },{
+      name: "JavaScript: DOM Manipulation",
+      url: "https://platzi.com/p/kastalco/curso/10135-course/diploma/detalle/",
+      color: "orange",
+      emoji: "https://static.platzi.com/media/achievements/badge-1-49b4ca10-ea84-48d4-ab6e-7ffa1475b68d.png",
+    },{
+      name: "CSS Architectures",
+      url: "https://platzi.com/p/kastalco/curso/7991-course/diploma/detalle/",
+      color: "sky",
+      emoji: "https://static.platzi.com/media/achievements/piezas-arquitecturas-css_badge-ebf283e7-d6da-4f7f-8d69-f2b7a37a146c.png",
     },
     {
-      name: "podcast",
-      url: "https://example.com/",
-      color: "yellow",
-      emoji: "💬",
+      name: "Git & Github",
+      url: "https://platzi.com/p/kastalco/curso/11059-course/diploma/detalle/",
+      color: "pink",
+      emoji: "https://static.platzi.com/media/achievements/badge-8-738d990a-87e0-488a-b069-6ac164a2790c.png",
+    },
+    {
+      name: "Basic Programming Course",
+      url: "https://platzi.com/p/kastalco/curso/13100-course/diploma/detalle/",
+      color: "slate",
+      emoji: "https://static.platzi.com/media/achievements/f7e396e6-942c-440c-8f1d-4e010739d363-4d13ac16-fe84-4d34-a2f9-a812cbba051d.png",
     },
   ],
   footer: "Made with Love in Colombia",
 };
 
+const main = ()=>{/* 
+    let name = document.createTextNode(data?.name); */
+    let links = data?.links?.map((link)=>{
+        return `<li class="bg-${link.color}-200 px-4 py-5 w-full flex items-center gap-2">
+                    <img class="w-8" src='${link.emoji}'>
+                    <a class="text-sm font-bold text-${link.color}-600 text-center hover:text-${link.color}-800 cursor-pointer"
+                        href="${link.url}" target="_blank">
+                        ${link.name}
+                    </a>
+                </li>`
+    }).join('');
+
+    let newItem = document.createElement("ul");
+    newItem.innerHTML = links;
+    $links.appendChild(newItem);
+
+    $name.textContent = data.name;
+
+    $userName.textContent = data.nickname;
+
+    $avatar.src = data.avatar;
+
+    $description.textContent = data.description;
+
+    setSocialMediaLinks();
+
+}
+
+function setSocialMediaLinks(){
+    const socialLinks = data?.social?.map((social)=>{
+        return `<li>
+                <a href="${social.url}" target="_blank">
+                    ${social.icon}
+                </a>
+                </li>`
+                
+    }).join('');
+
+
+    setSocialMediaContainer.innerHTML = socialLinks;
+
+}
+
+main();
