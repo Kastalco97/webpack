1aa5b659131fe55271b4c775095a956891132622
kastalco <kastalcowork@gmail.com>
2026-09-14 05:57:40 -0500

    Base HTML structure

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const $links = document.getElementById('links');
+const name = document.querySelector('h1');
+
+
+const data = {
+  name: "Kamilo Alomías",
+  nickname: "kastalco",
+  description: "...",
+  avatar: "...",
+  social: [
+    {
+      name: "linkedId",
+      url: "http://linkedin.com/in/kastalco",
+      username: "kastalco",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Kastalco97",
+      username: "Kastalco97",
+    },
+  ],
+  links: [
+    {
+      name: "blog",
+      url: "https://example.com/",
+      color: "red",
+      emoji: "📖",
+    },
+    {
+      name: "podcast",
+      url: "https://example.com/",
+      color: "yellow",
+      emoji: "💬",
+    },
+  ],
+  footer: "Made with Love in Colombia",
+};
+
